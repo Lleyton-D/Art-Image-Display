359467a45a3552b3ef622dc3e007f398378a536b
there <there@DAEMONLL_PC>
2024-07-19 23:13:58 +1000

    Add project files.

---
+++ Image Display/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Image_Display
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backButton = new System.Windows.Forms.Button();
+            this.pauseButton = new System.Windows.Forms.Button();
+            this.nextButton = new System.Windows.Forms.Button();
+            this.selectButton = new System.Windows.Forms.Button();
+            this.timerLabel = new System.Windows.Forms.Label();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // backButton
+            // 
+            this.backButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.backButton.Location = new System.Drawing.Point(1031, 821);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(75, 23);
+            this.backButton.TabIndex = 0;
+            this.backButton.Text = "<";
+            this.backButton.UseVisualStyleBackColor = true;
+            this.backButton.Click += new System.EventHandler(this.backButton_Click);
+            // 
+            // pauseButton
+            // 
+            this.pauseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.pauseButton.Location = new System.Drawing.Point(1112, 821);
+            this.pauseButton.Name = "pauseButton";
+            this.pauseButton.Size = new System.Drawing.Size(75, 23);
+            this.pauseButton.TabIndex = 1;
+            this.pauseButton.Text = "II";
+            this.pauseButton.UseVisualStyleBackColor = true;
+            this.pauseButton.Click += new System.EventHandler(this.pauseButton_Click);
+            // 
+            // nextButton
+            // 
+            this.nextButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.nextButton.Location = new System.Drawing.Point(1193, 821);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(75, 23);
+            this.nextButton.TabIndex = 2;
+            this.nextButton.Text = ">";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
+            // 
+            // selectButton
+            // 
+            this.selectButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.selectButton.Location = new System.Drawing.Point(109, 821);
+            this.selectButton.Name = "selectButton";
+            this.selectButton.Size = new System.Drawing.Size(75, 23);
+            this.selectButton.TabIndex = 3;
+            this.selectButton.Text = "Select";
+            this.selectButton.UseVisualStyleBackColor = true;
+            this.selectButton.Click += new System.EventHandler(this.selectButton_Click);
+            // 
+            // timerLabel
+            // 
+            this.timerLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.timerLabel.AutoSize = true;
+            this.timerLabel.Location = new System.Drawing.Point(31, 821);
+            this.timerLabel.Name = "timerLabel";
+            this.timerLabel.Size = new System.Drawing.Size(0, 13);
+            this.timerLabel.TabIndex = 4;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox.Location = new System.Drawing.Point(109, 40);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(1190, 773);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 5;
+            this.pictureBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1311, 854);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.timerLabel);
+            this.Controls.Add(this.selectButton);
+            this.Controls.Add(this.nextButton);
+            this.Controls.Add(this.pauseButton);
+            this.Controls.Add(this.backButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.Button pauseButton;
+        private System.Windows.Forms.Button nextButton;
+        private System.Windows.Forms.Button selectButton;
+        private System.Windows.Forms.Label timerLabel;
+        private System.Windows.Forms.PictureBox pictureBox;
+    }
+}
+
---
+++ Image Display/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Image_Display
+{
+    public partial class Form1 : Form
+    {
+        private int imageSelect = 0;
+        private string[] fileList;
+        private Timer timer;
+        public Form1()
+        {
+            InitializeComponent();
+            lastSelectedFolder = Properties.Settings.Default.LastSelectedFolder;
+        }
+        private string lastSelectedFolder = string.Empty;
+
+        private void selectButton_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                if (!string.IsNullOrEmpty(lastSelectedFolder))
+                {
+                    fbd.SelectedPath = lastSelectedFolder; // Set the last selected folder as the initial path
+                }
+
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    lastSelectedFolder = fbd.SelectedPath;
+                    fileList = Directory.GetFiles(fbd.SelectedPath);
+                    imageSelect = 0;
+                    displayImage();
+                    
+                    timer = new Timer();
+                    timer.Interval = 300000; // Set the interval to 2 seconds (2000 milliseconds)
+                    timerLabel.Text = "5:00";
+                    timer.Start();
+
+                    Properties.Settings.Default.LastSelectedFolder = lastSelectedFolder;
+                    Properties.Settings.Default.Save();
+                }
+            }
+        }
+
+        private void backButton_Click(object sender, EventArgs e)
+        {
+            imageSelect--;
+            displayImage();
+        }
+
+        private void pauseButton_Click(object sender, EventArgs e)
+        {
+            if(pauseButton.Text == "II")
+            {
+                timer.Stop();
+                pauseButton.Text = "▶";
+            }
+            else
+            {
+                timer.Start();
+                pauseButton.Text = "II";
+            }
+        }
+
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+            imageSelect++;
+            displayImage();
+        }
+        private void displayImage()
+        {
+
+            if (imageSelect < 0)
+                imageSelect = fileList.Length;
+            else if (imageSelect > fileList.Length)
+                imageSelect = 0;
+            
+            pictureBox.Image = Image.FromFile(fileList[imageSelect]);
+        }
+
+    }
+}
